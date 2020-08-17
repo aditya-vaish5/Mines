d612529b1ae4a6cbc3f550d01cdf8349d5eebbaf
Aditya Vaish <Aditya.Vaish@citrix.com>
2020-08-18 04:12:43 +0530

    Updates

---
+++ precache-manifest.4e7d398601a0e0df8920a4051d57fe65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d58579edf91b9e0db5268e36ead9cf0",
+    "url": "/mines/index.html"
+  },
+  {
+    "revision": "1bbc335afda76725fa43",
+    "url": "/mines/static/css/main.64db9a83.chunk.css"
+  },
+  {
+    "revision": "c3af1ae82082b6c000ca",
+    "url": "/mines/static/js/2.d623d390.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mines/static/js/2.d623d390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bbc335afda76725fa43",
+    "url": "/mines/static/js/main.43a109af.chunk.js"
+  },
+  {
+    "revision": "f61f612bcd4712f3d3a7",
+    "url": "/mines/static/js/runtime-main.bec3a66a.js"
+  }
+]);
